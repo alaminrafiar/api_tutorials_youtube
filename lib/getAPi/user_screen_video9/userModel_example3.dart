e9268d47680d7c1cf942af114bc760295c5f7584
Alamin <alamin35-449@diu.edu.bd>
2023-02-23 01:19:47 +0600

    user/name/email

--- lib/getAPi/user_screen_video9/userModel_example3.dart
+++ lib/getAPi/user_screen_video9/userModel_example3.dart
@@ -18,16 +18,15 @@ class _user_screenState extends State<user_screen> {
   Future<List<UserModel>> getUserApi() async {
     final response =
         await http.get(Uri.parse("https://jsonplaceholder.typicode.com/users"));
-   var data=jsonDecode(response.body.toString());
+    var data = jsonDecode(response.body.toString());
     if (response.statusCode == 200) {
-      for(Map i in data){
+      for (Map i in data) {
         userList.add(UserModel.fromJson(i));
         print(i["name"]);
       }
       return userList;
     } else {
       return userList;
-
     }
   }
 
@@ -39,7 +38,66 @@ class _user_screenState extends State<user_screen> {
         title: Text("Api courses Class 9 "),
       ),
       body: Column(
-        children: [],
+        children: [
+          Expanded(
+            child: FutureBuilder(
+              future: getUserApi(),
+              builder: (context, AsyncSnapshot<List<UserModel>> snapsot) {
+                if (!snapsot.hasData) {
+                  //snapsot ar age ! ta na dile aita sodo gorte thakbe
+                  return CircularProgressIndicator();
+                }
+                return ListView.builder(
+                  itemCount: userList.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Reusbalerow(title:"name",value:snapsot.data![index].name.toString()),
+                            Reusbalerow(title:"username",value:snapsot.data![index].username.toString()),
+                            Reusbalerow(title:"email",value:snapsot.data![index].email.toString()),
+                            Reusbalerow(title:"address",value:snapsot.data![index].address!.city.toString())
+                            //ai khane address alada class a ase ai jonno city & sathe  !null chek use kora hoise
+                            // Row(
+                            //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            //   children: [
+                            //     Text("name"),
+                            //     Text(snapsot.data![index].name.toString()),
+                              ],
+                            ),
+
+                        ),
+
+                    );
+                  },
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Reusbalerow extends StatelessWidget {
+  String title, value;
+
+  Reusbalerow({Key? key, required this.title, required this.value})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(title),
+          Text(value),
+        ],
       ),
     );
   }
